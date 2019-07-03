c2d924f753b9f82facb0fc66c4f4fb2ba7c2bfea
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2019-07-03 14:23:25 +0200

    controlled change in password and username

--- src/todo/TodoApp.jsx
+++ src/todo/TodoApp.jsx
@@ -11,11 +11,27 @@ class TodoApp extends Component {
 }
 
 class LoginComponent extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            username: 'username',
+            password: 'password'
+        }
+        this.handleUsernameChange = this.handleUsernameChange.bind(this)
+        this.handlePasswordChange = this.handlePasswordChange.bind(this)
+    }
+    handleUsernameChange(e) {
+        console.log(e.target.value);
+        this.setState( { username: e.target.value } )
+    }
+    handlePasswordChange(e) {
+        this.setState( { password: e.target.value } )
+    }
     render() {
         return (
             <div>
-                User Name: <input type="text" name="username"/>
-                Password: <input type="password" name="password"/>
+                User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleUsernameChange}/>
+                Password: <input type="password" name="password" value={this.state.password} onChange={this.handlePasswordChange}/>
                 <button>Login</button>
             </div>
         )
