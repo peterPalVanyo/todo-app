819cb7ca4bcb293de2e810c3c6234c94981ee2c6
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2019-07-03 15:52:23 +0200

    login message

--- src/todo/TodoApp.jsx
+++ src/todo/TodoApp.jsx
@@ -15,23 +15,40 @@ class LoginComponent extends Component {
         super(props)
         this.state = {
             username: 'username',
-            password: 'password'
+            password: 'password',
+            hasLoginFailed: false,
+            showSuccessMessage: false
         }
         this.handleChange = this.handleChange.bind(this)
+        this.loginClicked = this.loginClicked.bind(this)
     }
     handleChange(e) {
         console.log(e.target.value);
         this.setState( { [e.target.name]: e.target.value } )
     }
+    loginClicked(e) {
+        if(this.state.username === "username" && this.state.password === "alma") {
+            this.setState({showSuccessMessage: true})
+            this.setState({hasLoginFailed: false})
+        }
+        else {
+            this.setState({hasLoginFailed: true})
+            this.setState({showSuccessMessage: false})
+        }
+    }
     render() {
         return (
             <div>
+                {this.state.hasLoginFailed && <div>Invalid Credentials</div>}
+                {this.state.showSuccessMessage && <div>Login Sucessful</div>}
                 User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
                 Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
-                <button>Login</button>
+                <button onClick={this.loginClicked}>Login</button>
             </div>
         )
     }
 }
+
+
 
 export default TodoApp
