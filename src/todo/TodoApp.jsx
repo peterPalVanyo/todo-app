61903fe28f373c992912964e411aba5ca869cbc1
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2019-07-04 08:11:03 +0200

    auth service

--- src/todo/TodoApp.jsx
+++ src/todo/TodoApp.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
+import AuthenticationService from './AuthenticationService.js'
 
 class TodoApp extends Component {
     render() {
@@ -148,6 +149,7 @@ class LoginComponent extends Component {
     }
     loginClicked(e) {
         if(this.state.username === "username" && this.state.password === "alma") {
+            AuthenticationService.registerSuccessfulLogin(this.state.username, this.state.password);
             this.props.history.push(`/welcome/${this.state.username}`)
         }
         else {
