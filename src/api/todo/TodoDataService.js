3983aba18b68e92568c61984d479582e03a7a1b7
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2019-07-10 20:12:27 +0200

    jpa url

--- src/api/todo/TodoDataService.js
+++ src/api/todo/TodoDataService.js
@@ -1,25 +1,25 @@
 import Axios from "axios";
-import { API_URL } from "../../Constants"
+import { API_URL, JPA_API_URL } from "../../Constants"
 
 class TodoDataService {
     retrieveAllTodos(name) {
-        return Axios.get(`${API_URL}/users/${name}/todos`);
+        return Axios.get(`${JPA_API_URL}/users/${name}/todos`);
     }
 
     retrieveTodo(name, id) {
-        return Axios.get(`${API_URL}/users/${name}/todos/${id}`)
+        return Axios.get(`${JPA_API_URL}/users/${name}/todos/${id}`)
     }
 
     deleteTodo(name, id) {
-        return Axios.delete(`${API_URL}/users/${name}/todos/${id}`);
+        return Axios.delete(`${JPA_API_URL}/users/${name}/todos/${id}`);
     }
 
     updateTodo(name, id, todo) {
-        return Axios.put(`${API_URL}/users/${name}/todos/${id}`, todo);
+        return Axios.put(`${JPA_API_URL}/users/${name}/todos/${id}`, todo);
     }
 
     createTodo(name, todo) {
-        return Axios.post(`${API_URL}/users/${name}/todos/`, todo);
+        return Axios.post(`${JPA_API_URL}/users/${name}/todos/`, todo);
     }
 
 
